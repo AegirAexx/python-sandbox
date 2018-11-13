9152cdb5c8f2bf30def3ca0939b1e613650757d1
Ægir Æxx <aegir.tomasson@gmail.com>
2018-11-13 21:41:31 +0000

    Solved problem 8

--- programming_projects/problem08.py
+++ programming_projects/problem08.py
@@ -1,30 +1,12 @@
-''' Largest product in a series
-    The four adjacent digits in the 1000-digit number that have the
-    greatest product are 9 × 9 × 8 × 9 = 5832.
+""" Largest product in a series
 
-    73167176531330624919225119674426574742355349194934
-    96983520312774506326239578318016984801869478851843
-    85861560789112949495459501737958331952853208805511
-    12540698747158523863050715693290963295227443043557
-    66896648950445244523161731856403098711121722383113
-    62229893423380308135336276614282806444486645238749
-    30358907296290491560440772390713810515859307960866
-    70172427121883998797908792274921901699720888093776
-    65727333001053367881220235421809751254540594752243
-    52584907711670556013604839586446706324415722155397
-    53697817977846174064955149290862569321978468622482
-    83972241375657056057490261407972968652414535100474
-    82166370484403199890008895243450658541227588666881
-    16427171479924442928230863465674813919123162824586
-    17866458359124566529476545682848912883142607690042
-    24219022671055626321111109370544217506941658960408
-    07198403850962455444362981230987879927244284909188
-    84580156166097919133875499200524063689912560717606
-    05886116467109405077541002256983155200055935729725
-    71636269561882670428252483600823257530420752963450
+The four adjacent digits in the 1000-digit number that have the
+greatest product are 9 × 9 × 8 × 9 = 5832.
 
-    Find the thirteen adjacent digits in the 1000-digit number that
-    have the greatest product. What is the value of this product? '''
+Find the thirteen adjacent digits in the 1000-digit number that
+have the greatest product. What is the value of this product?"""
+
+from functools import reduce
 
 # =============
 # Program start
@@ -79,3 +61,35 @@ THOUSAND = tuple(THOUSAND_STRING)
 
 ''' The API function should take an integer that represents how many
 adjacent digits should be checked for the greatest product. '''
+
+# Defining a function to look at the 13 digits in the interval.
+
+
+def look_at_13(start):
+    # Using a list comprehension to get index of the tuple.
+    interval = list(i for i in range(start, start+13))
+    # Mapping the values from the indices in the list.
+    numbers = list(map(lambda n: int(THOUSAND[n]), interval))
+    # And finally reducing each 13 space interval to find its product.
+    return reduce(lambda acc, val: acc*val, numbers, 1)
+
+# The function that looks at the 1000 digit number.
+
+
+def look_at_1000():
+    # Two local variables to keep the highest seen product and its starting index.
+    highest = 0
+    index = 0
+    # The ranged loop to iterate the 1000 digits.
+    for val in range(988):
+        # Calling the interval reducer function.
+        current = look_at_13(val)
+        # Looking for the highest value and its starting index.
+        if current > highest:
+            highest = current
+            index = val
+    # Finally returning the greatest product and where it's seated.
+    return dict({'Hi-Product': highest, 'Interval': [index, index+12]})
+
+
+print(look_at_1000())
